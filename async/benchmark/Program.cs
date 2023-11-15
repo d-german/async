e445a207fc6afbdbf39708192cc3ec0da6338f7b
Damon German <damon.german@hyland.com>
2023-11-15 10:40:54 -0600

    Update benchmark tests for FibonacciBenchmarks
    The commit enhances the FibonacciBenchmarks class by adding array size parameters and two new benchmarks for testing average calculations: AvgTest and AvgAsParallelTest. It also initiates a global setup for test scenarios to generate a random array based on the defined array size. The Fibonacci benchmarks have been commented out as part of this update.

--- async/benchmark/Program.cs
+++ async/benchmark/Program.cs
@@ -2,21 +2,33 @@
 using BenchmarkDotNet.Running;
 
 namespace benchmark;
+
 [MemoryDiagnoser]
 public class FibonacciBenchmarks
 {
+    private int[] Values;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Values = GetRandomArray(ArraySize);
+    }
+
     // Define a parameter for the benchmark
-    [Params(40)] 
+    //[Params(10)]
     public long N;
 
-    [Benchmark]
+    [Params(1000, 10000, 100000)] // Array sizes to test
+    public int ArraySize;
+
+    //[Benchmark]
     public long BenchmarkFibonacci()
     {
         // Call the Fibonacci method with the benchmark parameter
         return Fibonacci(N);
     }
-    
-    [Benchmark]
+
+    //[Benchmark]
     public async Task<long> BenchmarkFibonacciAsync()
     {
         // Await the asynchronous Fibonacci method
@@ -28,7 +40,7 @@ public class FibonacciBenchmarks
         if (n is 0 or 1) return n;
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
-    
+
     private static Task<long> FibonacciAsync(long n)
     {
         // Your existing implementation of FibonacciAsync
@@ -37,18 +49,38 @@ public class FibonacciBenchmarks
             TaskCreationOptions.LongRunning,
             TaskScheduler.Default);
     }
+
+    [Benchmark]
+    public void AvgTest()
+    {
+        _ = Values.Average();
+    }
+
+    [Benchmark]
+    public void AvgAsParallelTest()
+    {
+        _ = Values.AsParallel().Average();
+    }
+
+    private int[] GetRandomArray(int size)
+    {
+        var random = new Random();
+        return Enumerable.Range(1, size)
+            .Select(x => random.Next(1, size))
+            .ToArray();
+    }
 }
 
 static class Program
 {
     static void Main(string[] args)
     {
-        var summary = BenchmarkRunner.Run<FibonacciBenchmarks>();
+        _ = BenchmarkRunner.Run<FibonacciBenchmarks>();
     }
 }
 
-
 /*
+
 | Method                  | N  | Mean      | Error     | StdDev    | Gen0   | Gen1   | Gen2   | Allocated |
 |------------------------ |--- |----------:|----------:|----------:|-------:|-------:|-------:|----------:|
 | BenchmarkFibonacci      | 15 |  2.463 us | 0.0299 us | 0.0265 us |      - |      - |      - |         - |
@@ -64,6 +96,17 @@ static class Program
 | BenchmarkFibonacci      | 50 | 79.83 s | 0.220 s | 0.195 s |    2.3 KB |
 | BenchmarkFibonacciAsync | 50 | 52.57 s | 0.125 s | 0.111 s |   2.55 KB |
 
+
+| Method            | ArraySize | Mean       | Error     | StdDev    | Gen0   | Allocated |
+|------------------ |---------- |-----------:|----------:|----------:|-------:|----------:|
+| AvgTest           | 1000      |   3.612 us | 0.0715 us | 0.0794 us |      - |      32 B |
+| AvgAsParallelTest | 1000      |  15.671 us | 0.1105 us | 0.1034 us | 1.0071 |   13304 B |
+
+| AvgTest           | 10000     |  35.910 us | 0.6812 us | 0.6690 us |      - |      32 B |
+| AvgAsParallelTest | 10000     |  27.174 us | 0.3897 us | 0.3645 us | 1.0071 |   13304 B |
+
+| AvgTest           | 100000    | 351.338 us | 6.2697 us | 6.1577 us |      - |      32 B |
+| AvgAsParallelTest | 100000    |  70.935 us | 0.8591 us | 0.7616 us | 0.9766 |   13304 B |
 
 
 */
